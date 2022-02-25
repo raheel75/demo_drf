1bb7df34d35b0b10c4469b881274b7428e7e1249
raheel <raheel.xovolt@gmail.com>
2022-02-25 15:50:00 +0500

    UserProfile is added

--- users/models.py
+++ users/models.py
@@ -11,3 +11,14 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    cell = models.CharField(max_length=15, default='')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cell
