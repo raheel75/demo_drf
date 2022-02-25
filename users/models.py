8b100f3cd00ce56b689600a1d994b49c2552d565
raheel <raheel.xovolt@gmail.com>
2022-02-25 16:00:01 +0500

    django rest framework is added

--- users/models.py
+++ users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
